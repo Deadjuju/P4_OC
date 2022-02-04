36a6bbe0c730919b043382415e6d1ed48998e479
Julien Garcia <julien.garcia-lp@laposte.net>
2022-02-04 12:13:17 +0100

    Ajouts des docstrings et simplification du code

--- controllers/playersmanager.py
+++ controllers/playersmanager.py
@@ -9,7 +9,12 @@ class Controller:
     def __init__(self, view):
         self.view = view
 
-    def _check_ask_create_player_or_upload_score(self):
+    def _check_ask_create_player_or_upload_score(self) -> str:
+        """User choice control which allows to select the option to execute
+
+                Returns:
+                    choice (str): the desired choice
+                """
         while True:
             choice = self.view.prompt_to_create_player_or_upload_ranking()
             if choice == "1":
@@ -24,7 +29,13 @@ class Controller:
                 print()
                 print("Merci de renseigner 1, 2 ou 3.")
 
-    def _ask_first_name(self):
+    def _ask_first_name(self) -> str:
+        """Ask and control of the first name field
+
+                Returns:
+                    first_name (str): first name
+                """
+
         while True:
             first_name = self.view.prompt_for_player_first_name().title()
             if first_name != "":
@@ -33,7 +44,13 @@ class Controller:
                 print()
                 print("Le champ - prénom - ne peut pas être vide.")
 
-    def _ask_last_name(self):
+    def _ask_last_name(self) -> str:
+        """Ask and control of the last name field
+
+                Returns:
+                    last_name (str): last name
+                """
+
         while True:
             last_name = self.view.prompt_for_player_last_name().title()
             if last_name != "":
@@ -42,7 +59,13 @@ class Controller:
                 print()
                 print("Le champ - nom de famille - ne peut pas être vide.")
 
-    def _ask_gender(self):
+    def _ask_gender(self) -> str:
+        """Ask and control of the gender field
+
+                Returns:
+                    gender (str): gender
+                """
+
         while True:
             gender = self.view.prompt_for_player_gender().lower()
             if gender == "femme" or gender == "female":
@@ -53,7 +76,13 @@ class Controller:
                 print()
                 print("Merci de renseigner un genre valide.")
 
-    def _ask_birthday(self):
+    def _ask_birthday(self) -> str:
+        """Control of the birthday field
+
+                Returns:
+                    date_of_birth (str): date of birth
+                """
+
         while True:
             date_of_birth = self.view.prompt_for_player_date_of_birth()
             try:
@@ -63,7 +92,13 @@ class Controller:
                 print()
                 print("La date doit être saisie au format jj/mm/aaaa")
 
-    def _create_player(self):
+    def _create_player(self) -> Player:
+        """Create an instance of Player
+
+                Returns:
+                    first_name (str): first name
+                """
+
         first_name = self._ask_first_name()
         last_name = self._ask_last_name()
         gender = self._ask_gender()
@@ -72,30 +107,42 @@ class Controller:
                         last_name=last_name,
                         gender=gender,
                         date_of_birth=date_of_birth)
-        if player.exists():
-            print("Ce joueur existe déjà.")
-            return
         return player
 
-    def _ask_save_player(self, player):
+    def _ask_save_player(self, player) -> str:
+        """Ask to register the instance of Player in database
+
+                Args:
+                    player (Player): BeautifulSoup object pointing to the table of characteristics
+
+                Returns:
+                    (str): yes or no
+                """
+
         return self.view.prompt_save_or_abort(message="Voulez-vous sauvegarder le joueur: ",
                                               subject=player.__dict__)
 
-    def _find_player_and_player_id(self):
+    def _find_players_list(self) -> list:
+        """Find in database the list of players who have the same characteristics
+
+                Returns:
+                    (list): list of players
+                """
+
         first_name = self._ask_first_name()
         last_name = self._ask_last_name()
         # list of players who have the same characteristics
         players = Player.search_player(first_name=first_name,
                                        last_name=last_name)
-        if len(players) == 1:
-            player = players[0]
-            self.view.show_the_player_found(player=True)
-            return player.doc_id, player
-        if len(players) == 0:
-            self.view.show_the_player_found(player=False)
-            return
+        return players
 
-    def _edit_or_delete_player(self):
+    def _edit_or_delete_player(self) -> str:
+        """Ask and control user choice: edit a player or delete it
+
+                Returns:
+                    (str): user choice
+                """
+
         while True:
             choice = self.view.prompt_to_edit_or_delete_player()
             if choice == "1":
@@ -106,25 +153,28 @@ class Controller:
                 print()
                 print("--> INFORMATION: Cette réponse n'est pas autorisé.")
 
-    def _edit_ranking_or_delete_a_player(self):
-        player_in_db = self._find_player_and_player_id()
-        if player_in_db is not None:
-            player_id = player_in_db[0]
-            player = player_in_db[1]
-            instance_player = Player(**player)
-            choice = self._edit_or_delete_player()
-            if choice == "edit":
-                new_ranking = self.view.prompt_to_change_score()
-                print(f"New score: {new_ranking}")
-                instance_player.update_a_ranking(new_ranking=new_ranking, player_id=player_id)
-                self.view.print_the_edit_delete_choice(choice=choice)
-                return
-            if choice == "delete":
-                instance_player.delete_a_player(player_id=player_id)
-                return
+    def _edit_ranking_or_delete_a_player(self, player):
+        """Make action to edit a player or to delete it
+
+                Args:
+                    player (): BeautifulSoup object pointing to the table of characteristics
+                """
+
+        player_id = player.doc_id
+        instance_player = Player(**player)
+        choice = self._edit_or_delete_player()
+        if choice == "edit":
+            new_ranking = self.view.prompt_to_change_score()
+            print(f"New score: {new_ranking}")
+            instance_player.update_a_ranking(new_ranking=new_ranking, player_id=player_id)
+            self.view.print_the_edit_delete_choice(choice=choice)
+            return
+        if choice == "delete":
+            instance_player.delete_a_player(player_id=player_id)
             return
 
     def players_manager(self):
+        """execution and selection of the different choices"""
         players_manager_run = True
         while players_manager_run:
             choice = self._check_ask_create_player_or_upload_score()
@@ -140,14 +190,22 @@ class Controller:
                 print("-" * 50)
                 print("Création d'un joueur")
                 player = self._create_player()
-                if player is not None:
+                if not player.exists():
                     if self._ask_save_player(player=player):
                         player.save()
+                else:
+                    print("Ce joueur existe déjà.")
 
             elif choice == "2":
                 print("-" * 50)
                 print("----MISE A JOUR----")
-                self._edit_ranking_or_delete_a_player()
+                players = self._find_players_list()
+                if len(players) == 1:
+                    player = players[0]
+                    self.view.show_the_player_found(player=True)
+                    self._edit_ranking_or_delete_a_player(player=player)
+                if len(players) == 0:
+                    self.view.show_the_player_found(player=False)
 
 
 if __name__ == "__main__":
