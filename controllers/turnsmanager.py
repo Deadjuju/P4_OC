303d7bba6d3986c08be3848258362739eb32c0fe
Julien Garcia <julien.garcia-lp@laposte.net>
2022-02-10 11:42:34 +0100

    Refactor et ajout docstrings de TurnsManager

--- controllers/turnsmanager.py
+++ controllers/turnsmanager.py
@@ -9,15 +9,37 @@ class TurnsManager(Controller):
 
     def __init__(self, view, tournament_name, turn_number: int, players_id_list: list):
         super().__init__(view)
-        self.view = view
         self.tournament_name = tournament_name
         self.turn_number = turn_number
         self.players_id_list = players_id_list
         self.pairs_generate = GenerationOfPairs(players_id_list=players_id_list)
 
-    def _checks_results(self, player):
+    def display_matchs(self, turn: Turn):
+        """Display each match: pairs of players
+
+                Args:
+                    turn (Turn): actual turn
+                """
+
+        self.view.show_the_matchs(matchs=turn.matchs)
+        while self.check_yes_or_no(
+                message="Voulez-vous revoir les matchs du tournois: ",
+                subject=self.tournament_name,
+                commit_message=("--- Voir les Matchs ---", "")
+        ):
+            self.view.show_the_matchs(matchs=turn.matchs)
+
+    def _ask_and_checks_results(self, player) -> float:
+        """ask to type a result from a player and check it
+
+                Args:
+                    player (): name of the player
+                Returns:
+                    (float): player score
+                """
+
         while True:
-            player_score = input(f"Score de {player}: ")
+            player_score = self.view.ask_question(question=f"Score de {player}: ")
             if player_score == "0":
                 return 0
             if player_score == "0.5" or player_score == "0,5":
@@ -28,7 +50,14 @@ class TurnsManager(Controller):
                 self.view.warning("Ce choix n'est pas autorisé.")
 
     @classmethod
-    def save_player_score(cls, player, player_score):
+    def _save_player_score(cls, player: str, player_score: float):
+        """upload the player score in database (Player)
+
+                Args:
+                    player (str): name of the player (unformatted name)
+                    player_score (float): player score
+                """
+
         first_name = player.split(" ")[1]
         last_name = player.split(" ")[2]
         players_list = Player.search_player(first_name=first_name,
@@ -37,32 +66,36 @@ class TurnsManager(Controller):
         player_id = player.doc_id
         instance_player = Player(**player)
         instance_player.tournament_score += player_score
-        print(instance_player.tournament_score)
-        print(first_name, last_name)
         instance_player.update_attribute(
             new_attribute_value=instance_player.tournament_score,
             attribute_name="tournament_score",
             player_id=player_id
         )
 
-    def ask_and_check_players_score(self, match):
+    def get_players_score(self, match: Match) -> tuple:
+        """Get score for each players from a match
+
+                Args:
+                    match (Match): Match's instance
+                """
 
         self.view.information(message="Rentrez\n"
                                       "0 -> pour une défaite\n"
                                       "0.5 -> pour un nul\n"
                                       "1 -> pour une victoire\n"
                                       f"----- {match} -----")
-        player1_score = self._checks_results(player=match.player_1)
-        self.save_player_score(player=match.player_1,
-                               player_score=player1_score)
+        player1_score = self._ask_and_checks_results(player=match.player_1)
+        self._save_player_score(player=match.player_1,
+                                player_score=player1_score)
 
-        player2_score = self._checks_results(player=match.player_2)
-        self.save_player_score(player=match.player_2,
-                               player_score=player2_score)
+        player2_score = self._ask_and_checks_results(player=match.player_2)
+        self._save_player_score(player=match.player_2,
+                                player_score=player2_score)
 
         return player1_score, player2_score
 
-    def turns_run(self):
+    def turns_run(self) -> Turn:
+        """creation of a turn and course of it """
         pairs = self.pairs_generate.swiss_system()
 
         matchs_list = [
@@ -73,33 +106,23 @@ class TurnsManager(Controller):
                     matchs=matchs_list,
                     current_turn_number=self.turn_number)
 
-        self.view.show_the_matchs(matchs=turn.matchs)
-        while self.check_yes_or_no(
-                message="Voulez-vous revoir les matchs du tournois: ",
-                subject=self.tournament_name,
-                commit_message=("--- Voir les Matchs ---", "")
-        ):
-            self.view.show_the_matchs(matchs=turn.matchs)
+        self.display_matchs(turn=turn)
 
         self.view.end_of_the_matchs()
+
         # Type the matchs results
         id_matchs_list = []
         for match in matchs_list:
-            match_results = self.ask_and_check_players_score(match=match)
+            match_results = self.get_players_score(match=match)
             # change score
             match.score_player_1 = match_results[0]
             match.score_player_2 = match_results[1]
             # save match end get id
             id_match = match.save()
             id_matchs_list.append(id_match)
-            print(f"ID MATCH: {id_match}")
 
         turn.matchs = id_matchs_list
 
-        # print(turn.__dict__)
-        # for match in turn.matchs:
-        #     print(match)
-        #
         return turn
 
 
