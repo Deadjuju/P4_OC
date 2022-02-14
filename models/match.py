40437dccfaaffaabe47a3605878a0ba7bdbf5070
Julien Garcia <julien.garcia-lp@laposte.net>
2022-02-14 15:11:50 +0100

    Ajout des docstrings de la classe Match

--- models/match.py
+++ models/match.py
@@ -1,12 +1,19 @@
-"""Match"""
+"""Modele Match"""
+
+from typing import Tuple, List
+
 from initialisation import MATCHS_TABLE
+from models.person import Player
+
+Player_PlayerScore = List[str]
 
 
 class Match:
+    """Match class: Two players with their score """
 
     def __init__(self,
-                 player_1,
-                 player_2,
+                 player_1: Player,
+                 player_2: Player,
                  score_player_1: float = 0,
                  score_player_2: float = 0):
         self.player_1 = player_1
@@ -22,16 +29,24 @@ class Match:
         return f"([{self.player_1}, {self.score_player_1}], [{self.player_2}, {self.score_player_2}])"
 
     @property
-    def match_name(self) -> tuple:
+    def match_name(self) -> Tuple[Player_PlayerScore, Player_PlayerScore]:
         """Returns the name of a match in the format ([player1, score1], [player2, score2])
             Returns:
-                (tuple): id of turn in database
+                (tuple): name of the match
                 """
 
-        return [{self.player_1}, {self.score_player_1}], [{self.player_2}, {self.score_player_2}]
+        return [f"{self.player_1}, {self.score_player_1}"], [f"{self.player_2}, {self.score_player_2}"]
 
     @classmethod
-    def get_matchs_instances_list(cls, matchs_id_list) -> list:
+    def get_matchs_instances_list(cls, matchs_id_list: List[int]) -> List:
+        """delete a player from database
+
+                Args:
+                    matchs_id_list (list): list of ids matchs
+                Returns:
+                    instances_matchs_list (list): list of Matchs Instances
+                """
+
         instances_matchs_list = []
         # get all turn's instance
         for match_id in matchs_id_list:
@@ -39,7 +54,6 @@ class Match:
             matchs_instance = Match(**match_dict)
             instances_matchs_list.append(matchs_instance)
         return instances_matchs_list
-
 
     def save(self) -> int:
         """ Save an instance of a match in the database
