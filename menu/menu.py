a638e7203afae878a5463eb52f7b621fa7b93c2d
Julien Garcia <julien.garcia-lp@laposte.net>
2022-02-15 15:03:22 +0100

    docstrings Menu

--- menu/menu.py
+++ menu/menu.py
@@ -7,23 +7,30 @@ from views.base import View
 
 
 class MenuView(View):
+    """Menu view"""
 
     @classmethod
     def prompt_for_menu_choice(cls):
+        """Prompts the user to choose an option
+            Returns:
+                    user_choice (str): User choice
+                """
+
         print("__________________________________________________\n"
               "__________________________________________________\n"
               "*********         MENU PRINCIPAL         *********\n"
               "__________________________________________________\n"
               "__________________________________________________\n")
-        choice = input("****    Pour gérer les joueurs, tapez '1'\n"
-                       "****    Pour gérer les tournois, tapez '2'\n"
-                       "****    Pour générer des rapports, tappez '3'\n"
-                       "****    Pour quitter, tappez 'off'"
-                       f"{View.CURSOR}  ")
-        return choice
+        user_choice = input("****    Pour gérer les joueurs, tapez '1'\n"
+                            "****    Pour gérer les tournois, tapez '2'\n"
+                            "****    Pour générer des rapports, tappez '3'\n"
+                            "****    Pour quitter, tappez 'off'"
+                            f"{View.CURSOR}  ")
+        return user_choice
 
 
 class MainMenu:
+    """ Menu controller """
 
     def __init__(self, view, player_view, tournament_view, reports_view):
         self.view = view
@@ -52,25 +59,26 @@ class MainMenu:
                 self.view.warning(message="Cette réponse n'est pas autorisé")
 
     def menu_run(self):
+        """execution and selection of the different menu choices"""
 
-        choice = self._ask_and_check_menu_choice()
+        menu_choices = self._ask_and_check_menu_choice()
 
-        if choice == "1":
+        if menu_choices == "1":
             player_control = PlayerController(view=self.player_view)
             player_control.players_manager()
 
-        if choice == "2":
+        if menu_choices == "2":
             tournament_control = TournamentController(view=self.tournament_view,
                                                       player_view=self.player_view)
             tournament_control.tournaments_manager()
 
-        if choice == "3":
+        if menu_choices == "3":
             report_control = ReportsController(view=self.reports_view,
                                                player_view=self.player_view,
                                                tournament_view=self.tournament_view)
             report_control.generate_a_report()
 
-        if choice == "off":
+        if menu_choices == "off":
             sys.exit()
 
 
@@ -79,13 +87,13 @@ if __name__ == '__main__':
     from views.tournament import TournamentView
     from views.reports import ReportsView
 
-    menu_view = MenuView
-    player_view = PlayerView()
-    tournament_view = TournamentView()
-    reports_view = ReportsView()
+    menu_view_test = MenuView
+    player_view_test = PlayerView()
+    tournament_view_test = TournamentView()
+    reports_view_test = ReportsView()
 
-    main_menu = MainMenu(view=menu_view,
-                         player_view=player_view,
-                         tournament_view=tournament_view,
-                         reports_view=reports_view)
+    main_menu = MainMenu(view=menu_view_test,
+                         player_view=player_view_test,
+                         tournament_view=tournament_view_test,
+                         reports_view=reports_view_test)
     main_menu.menu_run()
