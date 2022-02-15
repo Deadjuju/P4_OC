56226e9543bb74e6f73eceb3833eb10f7767a69c
Julien Garcia <julien.garcia-lp@laposte.net>
2022-02-15 18:28:38 +0100

    Ajout docstrings views/base

--- views/base.py
+++ views/base.py
@@ -2,21 +2,31 @@
 
 
 class View:
+    """ common views """
 
     CURSOR = "\n----->  || "
 
     @classmethod
-    def information(cls, message):
+    def information(cls, message: str):
+        """
+        layout of informative messages
+        :param message:
+        """
         print(f" ℹ️{'-' * 15} Information {'-' * 15} ℹ️\n"
               f"      ||| {message} |||\n")
 
     @classmethod
     def warning(cls, message):
+        """
+        layout of warning messages
+        :param message:
+        """
+
         print(f" ⚠️{'-' * 15} Attention {'-' * 15} ⚠️\n"
               f"      ||| {message} |||\n")
 
     @classmethod
-    def prompt_yes_or_no(cls):
+    def prompt_yes_or_no(cls) -> str:
         """Prompt Yes or No"""
         return input(f"Y/n{View.CURSOR}")
 
@@ -24,6 +34,3 @@ class View:
     def ask_question(cls, question):
         """Ask a question to user"""
         return input(question)
-
-
-
