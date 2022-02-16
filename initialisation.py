f3b15afc1ca0e24317e27b091104b7f0c8554ecf
Julien Garcia <julien.garcia-lp@laposte.net>
2022-02-16 09:25:00 +0100

    Mise en page

--- initialisation.py
+++ initialisation.py
@@ -3,11 +3,13 @@
 from tinydb import TinyDB
 from pathlib import Path
 
-DATE_FORMAT = ('%d/%m/%Y', 'jj/mm/aaaa')
-HOURS_FORMAT = "%Hh%M"
 
+# The number of rounds and players can be changed here
 DEFAULT_NUMBER_OF_TURNS = 4
 NUMBERS_OF_PLAYERS = 8
+
+DATE_FORMAT = ('%d/%m/%Y', 'jj/mm/aaaa')
+HOURS_FORMAT = "%Hh%M"
 
 # Save in DataBase at Roots
 DB = TinyDB(Path(__file__).resolve().parent / 'db.json', indent=4)
@@ -15,5 +17,3 @@ PLAYERS_TABLE = DB.table("Players")
 TOURNAMENTS_TABLE = DB.table("Tournaments")
 TURNS_TABLE = DB.table("Turns")
 MATCHS_TABLE = DB.table("Matchs")
-
-
